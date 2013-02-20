29f2262deea5f61ab4e609a5903cb06ce6e737e2
qiushanggao <qiushanggao@6121f079-4a18-41d3-8c30-8870a322f2c2>
2013-02-20 10:45:21 +0000

    Fix error dir name
    git-svn-id: http://192.168.20.11/sequoiadb/misc/ci@2291 6121f079-4a18-41d3-8c30-8870a322f2c2

--- mkrelease.sh
+++ mkrelease.sh
@@ -10,44 +10,44 @@ fi
 CODE_DIR=$1
 RELEASE_DIR=$2
 
-mkdir -p $RELEASE_DIR/sequioadb/bin
-mkdir -p $RELEASE_DIR/sequioadb/lib
-mkdir -p $RELEASE_DIR/sequioadb/include
-mkdir -p $RELEASE_DIR/sequioadb/samples
-mkdir -p $RELEASE_DIR/sequioadb/java
-mkdir -p $RELEASE_DIR/sequioadb/doc
-mkdir -p $RELEASE_DIR/sequioadb/conf/samples
-mkdir -p $RELEASE_DIR/sequioadb/conf/local
+mkdir -p $RELEASE_DIR/sequoiadb/bin
+mkdir -p $RELEASE_DIR/sequoiadb/lib
+mkdir -p $RELEASE_DIR/sequoiadb/include
+mkdir -p $RELEASE_DIR/sequoiadb/samples
+mkdir -p $RELEASE_DIR/sequoiadb/java
+mkdir -p $RELEASE_DIR/sequoiadb/doc
+mkdir -p $RELEASE_DIR/sequoiadb/conf/samples
+mkdir -p $RELEASE_DIR/sequoiadb/conf/local
 
 
-cp $CODE_DIR/sequoiadb $RELEASE_DIR/sequioadb/bin
-cp $CODE_DIR/sdb $RELEASE_DIR/sequioadb/bin
-cp $CODE_DIR/sdbbp $RELEASE_DIR/sequioadb/bin
-cp $CODE_DIR/sdbcm $RELEASE_DIR/sequioadb/bin
-cp $CODE_DIR/sdbstart $RELEASE_DIR/sequioadb/bin
-cp $CODE_DIR/sdbstop $RELEASE_DIR/sequioadb/bin
-cp $CODE_DIR/sdblist $RELEASE_DIR/sequioadb/bin
-cp $CODE_DIR/sdbinspt $RELEASE_DIR/sequioadb/bin
-cp $CODE_DIR/client/C/lib/*.so $RELEASE_DIR/sequioadb/lib
-cp $CODE_DIR/client/CPP/lib/*.so $RELEASE_DIR/sequioadb/lib
-mkdir -p $RELEASE_DIR/sequioadb/include/CPP/include
-mkdir -p $RELEASE_DIR/sequioadb/include/C/include
-cp $CODE_DIR/client/C/include/*.h $RELEASE_DIR/sequioadb/include/C/include
-cp $CODE_DIR/client/CPP/include/*.h $RELEASE_DIR/sequioadb/include/CPP/include
-cp -r $CODE_DIR/client/samples/*   $RELEASE_DIR/sequioadb/samples
+cp $CODE_DIR/sequoiadb $RELEASE_DIR/sequoiadb/bin
+cp $CODE_DIR/sdb $RELEASE_DIR/sequoiadb/bin
+cp $CODE_DIR/sdbbp $RELEASE_DIR/sequoiadb/bin
+cp $CODE_DIR/sdbcm $RELEASE_DIR/sequoiadb/bin
+cp $CODE_DIR/sdbstart $RELEASE_DIR/sequoiadb/bin
+cp $CODE_DIR/sdbstop $RELEASE_DIR/sequoiadb/bin
+cp $CODE_DIR/sdblist $RELEASE_DIR/sequoiadb/bin
+cp $CODE_DIR/sdbinspt $RELEASE_DIR/sequoiadb/bin
+cp $CODE_DIR/client/C/lib/*.so $RELEASE_DIR/sequoiadb/lib
+cp $CODE_DIR/client/CPP/lib/*.so $RELEASE_DIR/sequoiadb/lib
+mkdir -p $RELEASE_DIR/sequoiadb/include/CPP/include
+mkdir -p $RELEASE_DIR/sequoiadb/include/C/include
+cp $CODE_DIR/client/C/include/*.h $RELEASE_DIR/sequoiadb/include/C/include
+cp $CODE_DIR/client/CPP/include/*.h $RELEASE_DIR/sequoiadb/include/CPP/include
+cp -r $CODE_DIR/client/samples/*   $RELEASE_DIR/sequoiadb/samples
 ARCH=`arch`
 if [ "$ARCH"="x86_64" ]; then
-    cp -r $CODE_DIR/java/jdk_linux64   $RELEASE_DIR/sequioadb/java
+    cp -r $CODE_DIR/java/jdk_linux64   $RELEASE_DIR/sequoiadb/java
 #else arch
 fi
-cp $CODE_DIR/conf/samples/* $RELEASE_DIR/sequioadb/conf/samples/
+cp $CODE_DIR/conf/samples/* $RELEASE_DIR/sequoiadb/conf/samples/
  
-echo "find $RELEASE_DIR/sequioadb -name .svn -exec rm -rf {} \;"
-find $RELEASE_DIR/sequioadb -name .svn -exec rm -rf {} \;
+echo "find $RELEASE_DIR/sequoiadb -name .svn -exec rm -rf {} \;"
+find $RELEASE_DIR/sequoiadb -name .svn -exec rm -rf {} \;
 
-echo "tar -czf  $RELEASE_DIR/sequoiadb.tar.gz sequioadb"
+echo "tar -czf  $RELEASE_DIR/sequoiadb.tar.gz sequoiadb"
 cd $RELEASE_DIR/
-tar -czf  sequoiadb.tar.gz * sequioadb
+tar -czf  sequoiadb.tar.gz * sequoiadb
 echo "mv sequoiadb.tar.gz $RELEASE_DIR/sequoiadb.tar.gz"
 mv sequoiadb.tar.gz $RELEASE_DIR/sequoiadb.tar.gz
 
