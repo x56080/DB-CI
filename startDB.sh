e6f37663272d71223cb4eab86673df12985aadef
qiushanggao <qiushanggao@6121f079-4a18-41d3-8c30-8870a322f2c2>
2013-02-21 02:41:27 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/misc/ci@2297 6121f079-4a18-41d3-8c30-8870a322f2c2

--- startDB.sh
+++ startDB.sh
@@ -8,6 +8,14 @@ if [ $# -ne 1 ]; then
 fi
 
 DB_PATH=$1
+TAR_FILE_NAME=$2
+
+#unzip tar file
+cd $DB_PATH
+rm -rf $DB_PATH/sequoiadb
+tar -xzf $TAR_FILE_NAME .
+
+DB_PATH=$DB_PATH/sequoiadb
 
 #create data directory
 mkdir -p $DB_PATH/database/cata
@@ -16,9 +24,9 @@ mkdir -p $DB_PATH/database/log/cata
 mkdir -p $DB_PATH/database/log/data
 
 #copy configure file
-$DB_PATH/conf/local/sdb.cata.conf
-$DB_PATH/conf/local/sdb.data.conf
-$DB_PATH/conf/local/sdb.coord.conf
+mkdir -p $DB_PATH/conf/local/sdb.cata.conf
+mkdir -p $DB_PATH/conf/local/sdb.data.conf
+mkdir -p $DB_PATH/conf/local/sdb.coord.conf
 
 cp -f $DB_PATH/conf/samples/sdb.cat $DB_PATH/conf/local/sdb.cata.conf/sdb.cat
 cp -f $DB_PATH/conf/samples/sdb.conf.catalog $DB_PATH/conf/local/sdb.cata.conf/sdb.conf
