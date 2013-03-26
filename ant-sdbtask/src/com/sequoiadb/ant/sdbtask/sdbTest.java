94b7e4bc327d1dd982f06b147547f47140da603f
qiushanggao <qiushanggao@6121f079-4a18-41d3-8c30-8870a322f2c2>
2013-03-26 08:10:21 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/misc/ci@3369 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ ant-sdbtask/src/com/sequoiadb/ant/sdbtask/sdbTest.java
@@ -0,0 +1,81 @@
+/**
+ * 
+ */
+package com.sequoiadb.ant.sdbtask;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+import org.apache.tools.ant.types.Parameter;
+
+import com.ibm.staf.STAFException;
+import com.ibm.staf.STAFHandle;
+
+/**
+ * @author qiushanggao
+ *
+ */
+public class sdbTest  extends Task{
+	private String maxWaitTime = "30m";
+	
+	private String hostName = "localhost";
+	
+	private String scriptFileName;
+	
+	private List<Parameter> params = new ArrayList<Parameter>();
+	
+	public void setHost(String value)
+	{
+		hostName = value;
+	}
+	
+	public void setTestscript(String value)
+	{
+		scriptFileName = value;
+	}
+	
+	public void setTimeout(String value)
+	{
+		maxWaitTime = value;
+	}
+	
+	public Parameter createParam()
+	{
+		Parameter param = new Parameter();
+		params.add(param);
+		return param;
+	}
+	
+	
+	public void execute() {
+		try{
+			STAFHandle handle = new STAFHandle("ant-sdbtasks");
+			try{
+				//Staf PROCESS START  SHELL COMMAND  ant -l ${test.machine.deploy.path}/install-basic-in-host.log -f ${test.machine.deploy.path}/install-basic-in-host.xml -Dtest.basedir=${test.machine.deploy.path} -Ddeploy.filename=${deploy.tar.file.name} WORKDIR ${test.machine.deploy.path} WAIT 30m
+				
+				String request = "SHELL COMMAND ant -f " + scriptFileName;
+				
+				for(Parameter param: params)
+				{
+					request += " -D" + param.getName();
+					request += "=" + param.getValue();
+				}
+				
+				request += " WAIT " + maxWaitTime;
+				
+				handle.submit(hostName, "PROCESS", request);
+				
+			}
+			finally{
+				handle.unRegister();
+			}
+		}
+		catch (STAFException e)
+		{
+			e.printStackTrace();
+			throw new BuildException(e.toString());
+		}
+	}
+}
