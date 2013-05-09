ae99df0a1d0bde5b519074e61a2caeea361e096e
qiushanggao <qiushanggao@6121f079-4a18-41d3-8c30-8870a322f2c2>
2013-05-09 08:55:15 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/misc/ci@4711 6121f079-4a18-41d3-8c30-8870a322f2c2

--- ant-sdbtask/src/com/sequoiadb/ant/datatype/CataNodeGroup.java
+++ ant-sdbtask/src/com/sequoiadb/ant/datatype/CataNodeGroup.java
@@ -44,7 +44,7 @@ public class CataNodeGroup extends NodeGroup {
 						nodeInfo.getConfigMap());
 				
 				try {
-					Thread.sleep(5000);
+					Thread.sleep(20000);
 				} catch (InterruptedException e) {
 				}
 			} catch (BaseException e) {
