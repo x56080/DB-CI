864d0759bf8fe49e8dde180ad5a9236fadcdb154
qiushanggao <qiushanggao@6121f079-4a18-41d3-8c30-8870a322f2c2>
2013-05-22 07:53:36 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/misc/ci@5060 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ ant-sdbtask/src/com/sequoiadb/ant/datatype/DataRecord.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.sequoiadb.ant.datatype;
+
+import org.bson.BSONObject;
+import org.bson.util.JSON;
+
+/**
+ * @author qiushanggao
+ *
+ */
+public class DataRecord {
+
+	private String text = null;
+	
+	
+	public void addTest(String value)
+	{
+		text = value;
+	}
+	
+	public BSONObject toBSONObj()
+	{
+		BSONObject obj = null;
+		if (text != null)
+		{
+			obj = (BSONObject) JSON.parse(text);
+		}
+		return obj;
+	}
+}
