7fb027c804395705a165ecc68cc71a0daeb14ebc
chenzichuan <chenzichuan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2013-07-08 06:54:41 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/misc/ci@6231 6121f079-4a18-41d3-8c30-8870a322f2c2

---
+++ ant-sdbtask/src/com/sequoiadb/ant/sdbtask/createPrefix.java
@@ -0,0 +1,58 @@
+/**
+*
+*/
+package com.sequoiadb.ant.sdbtask;
+
+import java.io.File;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+import org.apache.tools.ant.types.Parameter;
+
+pubilc class createPrefix extends Task {
+
+   private String randomNumber ; 
+   
+   private List<Parameter> params = new ArrayList<Parameter>();
+   
+   public Parameter createParam() {
+		 
+		 Parameter param = new Parameter();
+		 
+		 params.add(param);
+		 
+		 return param;
+	 }
+   
+   public  void execute() {
+      
+      try{
+      	
+      	 String request = "" ;
+      	 
+      	 String lineNum = Integer.toString( (int)(Math.random()*1000) );
+      	 
+      	 for ( Parameter param : params ) {
+      	    
+      	    request += param.getValue() ; 	
+      	    
+      	 }
+      	 request += lineNum ; 
+      	 
+      	 this.getProject().setProperty( "CS_PRIX" , request.replaceAll( "[\-\_]" , "") ) ; 
+      	 
+      	
+      }catch (UnknownHostException e) {
+			
+			   e.printStackTrace();
+
+			   throw new BuildException(e.getMessage());
+		}
+   	
+   }
+	
+}
