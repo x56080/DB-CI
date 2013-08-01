0626a1ad7feb0d60a2e1881ed0ae94e775377c5f
chenzichuan <chenzichuan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2013-08-01 01:51:27 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/misc/ci@6922 6121f079-4a18-41d3-8c30-8870a322f2c2

--- ant-sdbtask/src/com/sequoiadb/ant/sdbtask/SdbNetWork.java
+++ ant-sdbtask/src/com/sequoiadb/ant/sdbtask/SdbNetWork.java
@@ -28,10 +28,10 @@ public class SdbNetWork extends Task {
 		final String hostName3 = "suse-test3";
 		final String hostName4 = "suse-test4";
 		String varHostName = value ;
-		if( value == hostName1) varHostName = "suse-test1.control" ; 
-		if( value == hostName2) varHostName = "suse-test2.control" ; 
-		if( value == hostName3) varHostName = "suse-test3.control" ; 
-		if( value == hostName4) varHostName = "suse-test4.control" ; 
+		if( value.equals( hostName1 ) ) varHostName = "suse-test1.control" ; 
+		if( value.equals( hostName2 ) ) varHostName = "suse-test2.control" ; 
+		if( value.equals( hostName3 ) ) varHostName = "suse-test3.control" ; 
+		if( value.equals( hostName4 ) ) varHostName = "suse-test4.control" ; 
 		
 		this.hostName = varHostName ; 
 	}
