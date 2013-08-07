ff6facc9182166b7322385f54496a58dd6c83431
chenzichuan <chenzichuan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2013-08-07 12:56:00 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/misc/ci@7054 6121f079-4a18-41d3-8c30-8870a322f2c2

--- ant-sdbtask/src/com/sequoiadb/ant/sdbtask/SdbNetWork.java
+++ ant-sdbtask/src/com/sequoiadb/ant/sdbtask/SdbNetWork.java
@@ -48,30 +48,30 @@ public class SdbNetWork extends Task {
 			handle = new STAFHandle("ant-sdbtasks");
 			
 			//file_string is a shell program , and it will be wrode in killNIC.sh file
-			String file_string = "nicName=`ifconfig | grep eth | awk '{print $1}'` ; \\n " +
-					" portName=`ifconfig | grep addr:192.168 | awk '{print $2}'` ; \\n " +
+			String file_string = "nicName=\\`ifconfig | grep eth | awk '{print $1}'\\` ; \\n " +
+					" portName=\\`ifconfig | grep addr:192.168 | awk '{print \\$2}'\\` ; \\n " +
 					" i=0 ; \\n " +
-					"for list in $portName  \\n " +
+					"for list in \\$portName  \\n " +
 					"do \\n " + 
-					"echo $list | grep 192.168.30 ; \\n " + 
-					"if [ $? -eq 0 ] ; then \\n " + 
-					" getNic=${nicName:i*5:i+4} \\n " + 
+					"echo \\$list | grep 192.168.30 ; \\n " + 
+					"if [ \\$? -eq 0 ] ; then \\n " + 
+					" getNic=\\${nicName:i*5:i+4} \\n " + 
 					"fi \\n " + 
 					"let i++; \\n " + 
 					"done ; \\n " + 
-					"if [ ! -n $getNic ] ; then \\n " +
+					"if [ !   -n \\$getNic ] ; then \\n " +
 					" echo fail to getNic Name ; \\n " +
 					" exit 1 ; \\n " + 
 					"fi \\n " ; 
-			file_string += " ifconfig $getNic  " + this.upOrDown + "  ; \\n " 
-					+ "if [ $? -ne 0 ] ; then \\n "
-					+ " echo fail to ifconfig $getNic " + this.upOrDown + " ; \\n "
+			file_string += " ifconfig \\$getNic  " + this.upOrDown + "  ; \\n " 
+					+ "if [ \\$?  -ne  0 ] ; then \\n "
+					+ " echo fail to ifconfig \\$getNic " + this.upOrDown + " ; \\n "
 					+ " exit 1 ; \\n " 
 					+ "fi \\n " ;
 			
 			doWork = "base_dir=`pwd` ; "  
 					+ "touch $base_dir/killNIC.sh ; " 
-					+ "echo -e " + file_string + ">>$base_dir/killNIC.sh ; " 
+					+ "echo -e \"" + file_string + "\">>$base_dir/killNIC.sh ; " 
 					+ "chmod a+x $base_dir/killNIC.sh ; "
 					+ "$base_dir/killNIC.sh ; " ;
 					//+ "rm $base_dir/killNIC.sh ; " ; 
