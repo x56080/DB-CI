4aaae5c4334b7d31ffb1bdb5b75d361d44bd1238
chenzichuan <chenzichuan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2013-09-16 07:37:44 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/misc/ci@7783 6121f079-4a18-41d3-8c30-8870a322f2c2

--- exec-tools/print_version.c
+++ exec-tools/print_version.c
@@ -2,6 +2,9 @@
 #include "ossVer.h"
 int main()
 {
+	 /*
+	 * this file just to print the software version on the screen for ant to catch 
+	 */
    printf("%d.%d",SDB_ENGINE_VERISON_CURRENT , SDB_ENGINE_SUBVERSION_CURRENT);
    return 0;
 }
