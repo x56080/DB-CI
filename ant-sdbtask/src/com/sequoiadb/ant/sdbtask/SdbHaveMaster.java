00bb60e08956c156452557dda29254bf2c4daf04
chenzichuan <chenzichuan@6121f079-4a18-41d3-8c30-8870a322f2c2>
2013-10-21 08:00:07 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/misc/ci@8583 6121f079-4a18-41d3-8c30-8870a322f2c2

--- ant-sdbtask/src/com/sequoiadb/ant/sdbtask/SdbHaveMaster.java
+++ ant-sdbtask/src/com/sequoiadb/ant/sdbtask/SdbHaveMaster.java
@@ -1,123 +1,132 @@
 package com.sequoiadb.ant.sdbtask;
 
+
 import org.apache.tools.ant.Task;
 import org.bson.BSONObject;
 import org.bson.types.BasicBSONList;
 import com.sequoiadb.base.ReplicaGroup;
 import com.sequoiadb.base.Sequoiadb;
 import com.sequoiadb.exception.BaseException;
-
 /**
- * @author
+ * @author 
  */
 public class SdbHaveMaster extends Task {
-	private String hostName = "localhost";
-	private String port = "50000";
-	private String groupName = null;
-	private String waitTime = "120";
-	private String propertyName = null;
-
-	public void setPropertyName(String value) {
-		this.propertyName = value;
+	private String hostName = "localhost" ;
+	private String port = "50000" ; 
+	private String groupName = null ; 
+	private String waitTime = "120" ; 
+	private String propertyName= null ;
+	private static int ERROR_STANTALONG=-159;
+	
+	
+	public void setPropertyName( String value )
+	{
+		this.propertyName = value ;
 	}
-
-	public void setPort(String value) {
-		this.port = value;
+	public void setPort( String value )
+	{
+		this.port = value ; 
 	}
-
-	public void setHostName(String value) {
-		this.hostName = value;
+	public void setHostName( String value )
+	{
+		this.hostName = value ; 
 	}
-
-	public void setGroupName(String value) {
-		if (value.equals("1"))
+	public void setGroupName( String value )
+	{
+		if( value.equals("1") )
 			this.groupName = "SYSCatalogGroup";
 		else
-			this.groupName = value;
+			this.groupName = value ;
 	}
-
-	public void setWaitTime(String value) {
-		this.waitTime = value;
+	public void setWaitTime( String value )
+	{
+		this.waitTime = value ; 
 	}
-
-	private boolean checkMaster(ReplicaGroup RG, Sequoiadb sdb) {
-
-		try {
-			// GroupID ;
-			String groupID = RG.getDetail().get("GroupID").toString();
-			int nodeNum = RG.getNodeNum(null);
-			// System.out.println( RG.getNodeNum(null) ) ;
-			BasicBSONList bson_list = (BasicBSONList) RG.getDetail().get(
-					"Group");
-
-			for (int i = 0; i < nodeNum; i++) {
-				try {
-					BSONObject oneBson = (BSONObject) bson_list.get(i);
-					String nodeID = oneBson.get("NodeID").toString();
-					if (sdb.getSnapshot(7,
-							"{GroupID:" + groupID + ",NodeID:" + nodeID + "}",
-							"{\"IsPrimary\":null}", null).hasNext()) {
-						String isMaster = sdb
-								.getSnapshot(
-										7,
-										"{GroupID:" + groupID + ",NodeID:"
-												+ nodeID + "}",
-										"{\"IsPrimary\":null}", null).getNext()
-								.get("IsPrimary").toString();
-						if (isMaster.equals("true"))
-							return true;
+	
+	private boolean checkMaster( ReplicaGroup RG , Sequoiadb sdb )
+	{
+		
+			
+			//GroupID  ;
+			String groupID = RG.getDetail().get( "GroupID" ).toString() ;
+			int nodeNum = RG.getNodeNum(null) ; 
+			//System.out.println( RG.getNodeNum(null) ) ;
+			BasicBSONList bson_list = (BasicBSONList)RG.getDetail().get("Group") ;
+			
+			for(int i = 0 ; i < nodeNum ; i++ )
+			{
+				try{
+					BSONObject oneBson = (BSONObject) bson_list.get( i ) ; 
+					String nodeID = oneBson.get( "NodeID" ).toString() ;
+					if( sdb.getSnapshot(7,"{GroupID:"
+							+ groupID + ",NodeID:"
+							+ nodeID + "}"
+							, "{\"IsPrimary\":null}"
+							, null).hasNext() )
+					{
+						String isMaster = sdb.getSnapshot(7,"{GroupID:"
+									+ groupID + ",NodeID:"
+									+ nodeID + "}"
+									, "{\"IsPrimary\":null}"
+									, null)
+									.getNext().get("IsPrimary").toString() ;
+						if( isMaster.equals( "true" ) )
+							return true ;
 					}
-				} catch (BaseException e) {
-					continue;
-				}
+				}catch( BaseException e ){}
+				
 			}
-		} catch (BaseException e) {
-			return false;
-		}
-
-		//
-		return false;
+		
+		
+	//	
+		return false; 
 	}
-
-	public void execute() {
-		Sequoiadb sdb = new Sequoiadb(this.hostName,
-				Integer.parseInt(this.port), "", "");
-		ReplicaGroup RG = null;
-		int i = 0;
-		int times = Integer.parseInt(this.waitTime);
-		for (; i < times; ++i) {
-			try {
-				if (!this.groupName.equals("1"))
-					RG = sdb.getReplicaGroup(this.groupName);
+	
+	public void execute ()
+	{
+		Sequoiadb sdb = new Sequoiadb( this.hostName , Integer.parseInt( this.port ) , "" ,"") ;
+		ReplicaGroup RG = null ;
+		int i = 0 ;
+		int times = Integer.parseInt( this.waitTime ) ;
+		for(; i < times ; ++i )
+		{
+			try{
+				if( ! this.groupName.equals("1") )
+					RG = sdb.getReplicaGroup( this.groupName ) ;
 				else
-					RG = sdb.getReplicaGroup(1);
-				if (RG != null)
+					RG = sdb.getReplicaGroup(1) ;
+				if( RG != null )
 					break;
-			} catch (BaseException e) {
+			}catch( BaseException e ){ 
 				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
+					if( e.getErrorCode() == ERROR_STANTALONG )
+						this.getProject().setProperty( this.propertyName , "true" ) ;
+				   Thread.sleep(1000) ;
+			    } catch (InterruptedException e1) {
+				   // TODO Auto-generated catch block
+				   e1.printStackTrace();
+			    }
 			}
 		}
-
-		for (; i < times; ++i) {
-			if (false == checkMaster(RG, sdb))
+		
+		
+		for(; i < times ; ++i )
+		{
+			if( false == checkMaster( RG , sdb ) )
 				try {
 					Thread.sleep(1000);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-			else {
-				this.getProject().setProperty(this.propertyName, "true");
-				break;
+			else
+			{	
+				this.getProject().setProperty( this.propertyName , "true" ) ;
+				break ;
 			}
 		}
-		if (!(i < times))
-			this.getProject().setProperty(this.propertyName, "false");
+		if( ! (i < times) )
+			this.getProject().setProperty( this.propertyName , "false" ) ;
 	}
 
 }
