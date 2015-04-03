4e57d40dc8f9b5d718bf159e56b25600656e40dc
longyang <longyang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2015-04-03 08:41:07 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk/misc/ci@18115 6121f079-4a18-41d3-8c30-8870a322f2c2

--- exec-tools/print_version.c
+++ exec-tools/print_version.c
@@ -5,6 +5,11 @@ int main()
 	 /*
 	 * this file just to print the software version on the screen for ant to catch 
 	 */
-   printf("%d.%d",SDB_ENGINE_VERISON_CURRENT , SDB_ENGINE_SUBVERSION_CURRENT);
+#ifdef SDB_ENGINE_FIXVERSION_CURRENT
+   printf("%d.%d.%d",SDB_ENGINE_VERISON_CURRENT , SDB_ENGINE_SUBVERSION_CURRENT,
+          SDB_ENGINE_FIXVERSION_CURRENT );
+#else
+   printf("%d.%d",SDB_ENGINE_VERISON_CURRENT , SDB_ENGINE_SUBVERSION_CURRENT );
+#endif // SDB_ENGINE_FIXVERSION_CURRENT
    return 0;
 }
