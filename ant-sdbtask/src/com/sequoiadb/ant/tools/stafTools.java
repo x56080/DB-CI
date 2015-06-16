b020a7fc3ddd23e92f3054704e264fc2d4dd85e8
longyang <longyang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2015-06-16 08:29:35 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk/misc/ci@19249 6121f079-4a18-41d3-8c30-8870a322f2c2

--- ant-sdbtask/src/com/sequoiadb/ant/tools/stafTools.java
+++ ant-sdbtask/src/com/sequoiadb/ant/tools/stafTools.java
@@ -89,12 +89,10 @@ public class stafTools extends Task{
 				System.out.println("exec: staf " + workHost+ " PROCESS " + request);
 				result = handle.submit2( workHost , "PROCESS", request);
 
-				System.out.println("resultContext : " + result.result);
 				System.out.println("result.rc : " + result.rc);
 				System.out.println("result.Ok : " + result.Ok);
 
 				if( failonerror){
-					result.rc = 1;
 					if (result.rc != result.Ok) {
 						throw new BuildException(STAFResultToString(result));
 					}else{
