ab01bb9fa7486eefd0df5c9af4ccfae56b6a0d74
longyang <longyang@6121f079-4a18-41d3-8c30-8870a322f2c2>
2015-07-29 07:11:04 +0000

    git-svn-id: http://192.168.20.11/sequoiadb/trunk/misc/ci@19844 6121f079-4a18-41d3-8c30-8870a322f2c2

--- ant-sdbtask/src/com/sequoiadb/ant/sdbtask/SdbCopyAndTar.java
+++ ant-sdbtask/src/com/sequoiadb/ant/sdbtask/SdbCopyAndTar.java
@@ -89,6 +89,15 @@ public class SdbCopyAndTar extends Task{
 				+ "fi \n"
 				+ "} \n"
 				+ "\n"
+				+ "checkConf()\n"
+				+ "{\n"
+				+ "if test -d $1/../conf/local\n"
+				+ "then\n"
+				+ "   echo $1/../conf/local is exit ;\n"
+				+ "   cp -r \"$1/../conf/local\"  \"$2\" ;\n"
+				+ "fi\n"
+				+ "}\n"
+				+ "\n"
 				+ "if test -d $1 \n"
 				+ "then \n"
 				+ "   BASE_DIR=$(readlink -f $0) ; \n"
@@ -97,12 +106,12 @@ public class SdbCopyAndTar extends Task{
 				+ "   mkdir -p $BASE_DIR\"/" + this.filehostName + "-diaglog\" ; \n"
 				+ "   DIAL_DIR=$BASE_DIR\"/" + this.filehostName + "-diaglog\" ; \n"
 				+ "   checkDiaglog $1 $DIAL_DIR ; \n"
+				+ "   checkConf $1 $DIAL_DIR ; \n"
 				+ "fi \n"
 				+ "\n"
 				+ "cd $BASE_DIR ; \n"
 				+ "\n"
 				+ "tar -zcvf $BASE_DIR/" + this.filehostName + "-diaglog.tar.gz  " + this.filehostName+ "-diaglog ; \n" 
-//				+ "mv $BASE_DIR/" + file_name + "-diaglog.tar.gz  ${BASE_DIR}/" + file_name + "-diaglog.tar.gz ; \n"
 				+ ""
 				+ ""
 				+ ""
@@ -121,9 +130,7 @@ public class SdbCopyAndTar extends Task{
 			String request = null ;
 			STAFResult result = null ;
 
-			String now_dir = System.getProperty("user.dir") ;
-			
-			 
+			String now_dir = System.getProperty("user.dir") ;	 
 			String fileName = now_dir + "/shWork.sh" ;
 			File file = new File( fileName ) ;
 			if( ! file.exists() ){
@@ -141,8 +148,7 @@ public class SdbCopyAndTar extends Task{
 			doWork = " chmod a+x  " + this.diaglogPath + "/shWork.sh ; "
 					+ "  " 
 					+ this.diaglogPath + "/shWork.sh   " + this.diaglogPath + " ; " ; 
-					//+ " rm  " + this.diaglogPath + "/shWork.sh ; " ; 
-			
+
 			handle = new STAFHandle("ant-sdbtasks");
 			
 			request = "  COPY FILE   " + fileName + "    TODIRECTORY  " + this.diaglogPath 
@@ -153,12 +159,9 @@ public class SdbCopyAndTar extends Task{
 			if (result.rc != STAFResult.Ok) {
 				throw new BuildException(STAFResultToString(result));
 			}
-			
-			
+						
 			log(" shWork.sh file : \n" + this.write_file1() ) ;
-			
-			//copyPath = this.diaglogPath ;
-			
+
 			request = "START SHELL COMMAND " + doWork + " WAIT 30m " ; 
 			
 			log("exec: staf " + this.filehostName + " PROCESS " + request);
@@ -167,28 +170,7 @@ public class SdbCopyAndTar extends Task{
 			if (result.rc != STAFResult.Ok) {
 				throw new BuildException(STAFResultToString(result));
 			}
-			
-				/*
-				Runtime run = Runtime.getRuntime() ;
-				System.out.println("This is tarPath choose"); 
-				log( "tar -zcvf  " + this.filehostName + "-diaglog.tar.gz"
-						+ this.buildNum 
-						+ "  " + this.tarPath + "/ " ) ; 
-				String str[] = {"/bin/sh"
-						, "-c"
-						, "sh tar -zcvf  " + this.tarPath + "/" + this.filehostName + "-diaglog.tar.gz"
-								+ this.buildNum 
-								+ "  " + this.tarPath + "/* "
-				};
-				run.exec(str);
-				//Process pro = run.exec( str ) ;
-				//BufferedReader br = new BufferedReader(new InputStreamReader( pro.getInputStream() ) ) ;
-				//String killpid = br.readLine() ;
-				
-				copyPath = this.tarPath ;
-				*/			
-			
-			
+
 			request = "COPY FILE  " 
 					+ this.diaglogPath + "/" + this.filehostName + "-diaglog.tar.gz" + this.buildNum 
 					+ " TODIRECTORY " + this.savePath + " TOMACHINE "
@@ -197,25 +179,9 @@ public class SdbCopyAndTar extends Task{
 			log("exec: staf " + this.filehostName + " FS " + request);
 			result = handle.submit2(this.filehostName, "FS", request);
 
-			//log(STAFResultToString(result));
 			if (result.rc != STAFResult.Ok) {
 				throw new BuildException(STAFResultToString(result));
 			}
-			/*
-			request = "DELETE ENTRY " + copyPath + "/" + this.hostName + "-diaglog.tar.gz" 
-					+ " RECURSE CONFIRM";
-			log("exec: staf " + this.hostName + " FS " + request);
-			result = handle.submit2(this.hostName, "FS", request);
-
-			log(STAFResultToString(result));
-			if (result.rc != STAFResult.Ok) {
-				throw new BuildException(STAFResultToString(result));
-			}
-			
-			if( ! file.delete() ){
-				throw new Exception( "delete shWork.sh file fail" ) ; 
-			}
-			*/
 		}catch (STAFException e) {
 			String errorMsg = "STAFException, RC=" + e.rc + "\nmsg="
 					+ e.getLocalizedMessage();
