bcc4b5b8f6afa3f8da7a0d968eb44c52339b4b4d
yuting <yuting@6121f079-4a18-41d3-8c30-8870a322f2c2>
2016-11-14 02:03:50 +0000

    编目节点打开事务
    git-svn-id: http://192.168.20.11/sequoiadb/trunk/misc/ci@25641 6121f079-4a18-41d3-8c30-8870a322f2c2

--- deploy-config/deploy.js
+++ deploy-config/deploy.js
@@ -111,7 +111,8 @@ function deployClster( mode )
                   sharingbreak:30000,
                   diagnum:30,
                   optimeout:60000,
-                  fap:fapValue
+                  fap:fapValue,
+                  transactionon:true
                 };
    db.createCataRG( controlHost, cataPort, 
                     databaseDir+"/cata/"+cataPort,
