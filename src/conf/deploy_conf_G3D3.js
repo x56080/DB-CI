a3e58e17f120597ccf65c07a36dcb994a761c43d
chensiqin <chensiqin@6121f079-4a18-41d3-8c30-8870a322f2c2>
2019-03-27 08:52:26 +0000

    撤销上一步操作
    git-svn-id: http://192.168.20.11/sequoiadb/trunk/misc/ci@40070 6121f079-4a18-41d3-8c30-8870a322f2c2

--- src/conf/deploy_conf_G3D3.js
+++ src/conf/deploy_conf_G3D3.js
@@ -47,6 +47,5 @@ var dataConf  = { diaglevel:diagLevel,
                   diagnum:30,
                   logfilenum:40,
                   plancachelevel:3,
-                  auditmask:"SYSTEM|DDL|DCL|DML",
                   fap:fapValue
                 };
