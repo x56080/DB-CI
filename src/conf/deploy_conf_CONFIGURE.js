ebf6f56b376a2b7711665becb73c5c7081194dbc
chensiqin <47-chensiqin@users.noreply.gitlab.example.com>
2020-03-24 14:28:21 +0800

    fapmongo改为fapmongo2

--- src/conf/deploy_conf_CONFIGURE.js
+++ src/conf/deploy_conf_CONFIGURE.js
@@ -23,7 +23,7 @@ if( typeof( diagLevel ) === "undefined" )
 var osArch = new Cmd().run('arch');
 if ( osArch === "x86_64\n" ) 
 { 
-   var fapValue = "fapmongo";
+   var fapValue = "fapmongo2";
 }
 else
 {
@@ -33,8 +33,7 @@ else
 var cataConf  = { diaglevel:diagLevel,
                   sharingbreak:30000,
                   diagnum:60,
-                  logfilenum:40,
-                  fap:fapValue
+                  logfilenum:40
                 };
 var coordConf = { diaglevel:diagLevel,
                   diagnum:60,
@@ -45,6 +44,5 @@ var dataConf  = { diaglevel:diagLevel,
                   sharingbreak:30000,
                   diagnum:60,
                   logfilenum:40,
-                  dmschkinterval:1,
-                  fap:fapValue
+                  dmschkinterval:1
                 };
