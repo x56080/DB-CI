3136b3a96030aa61568adbf470ad309f16bf220d
chensiqin <47-chensiqin@users.noreply.gitlab.example.com>
2020-03-24 14:30:33 +0800

    fapmongo改为fapmongo2

--- src/conf/deploy_conf_STANDALONE.js
+++ src/conf/deploy_conf_STANDALONE.js
@@ -23,7 +23,7 @@ if( typeof( diagLevel ) === "undefined" )
 var osArch = new Cmd().run('arch');
 if ( osArch === "x86_64\n" ) 
 { 
-   var fapValue = "fapmongo";
+   var fapValue = "fapmongo2";
 }
 else
 {
@@ -32,6 +32,5 @@ else
 
 var nodeConf  = { diaglevel:diagLevel,
                   diagnum:60,
-                  logfilenum:40,
-                  fap:fapValue
+                  logfilenum:40
                 };
