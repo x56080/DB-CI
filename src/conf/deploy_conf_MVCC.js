706861164ae4f46d2bcbc2c6d805d1d2e6b67f1e
yaoyiming <yaoyiming@sequoiadb.com>
2023-09-07 14:25:38 +0800

    CI2425修改部署配置fapmongo2->fapmongo3

--- src/conf/deploy_conf_MVCC.js
+++ src/conf/deploy_conf_MVCC.js
@@ -23,7 +23,7 @@ if( typeof( diagLevel ) === "undefined" )
 var osArch = new Cmd().run('arch');
 if ( osArch === "x86_64\n" ) 
 { 
-   var fapValue = "fapmongo2";
+   var fapValue = "fapmongo3";
 }
 else
 {
