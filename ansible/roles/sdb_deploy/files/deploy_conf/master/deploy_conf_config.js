29b0734886b7a2440d1c0c08ebe6a9c5d377b2c1
wangwenjing <wangwenjing@sequoiadb.com>
2024-02-08 14:22:40 +0800

    启用netcompressor配置

--- ansible/roles/sdb_deploy/files/deploy_conf/master/deploy_conf_config.js
+++ ansible/roles/sdb_deploy/files/deploy_conf/master/deploy_conf_config.js
@@ -34,13 +34,15 @@ var cataConf  = { diaglevel:diagLevel,
                   sharingbreak:30000,
                   diagnum:60,
                   ftfusingtimeout:300,
-                  logfilenum:40
+                  logfilenum:40,
+                  netcompressor:'LZ4'
                 };
 var coordConf = { diaglevel:diagLevel,
                   diagnum:60,
                   logfilenum:40,
                   ftfusingtimeout:300,
-                  fap:fapValue
+                  fap:fapValue,
+                  netcompressor:'LZ4'
                 };
 var dataConf  = { diaglevel:diagLevel,
                   sharingbreak:30000,
@@ -49,5 +51,6 @@ var dataConf  = { diaglevel:diagLevel,
                   ftfusingtimeout:300,
                   dmschkinterval:1,
                   indexpath: METADATA_DIR + '/[svcname]/',
-                  lobmetapath:  METADATA_DIR + '/[svcname]/'
+                  lobmetapath:  METADATA_DIR + '/[svcname]/',
+                  netcompressor:'LZ4'
                 };
