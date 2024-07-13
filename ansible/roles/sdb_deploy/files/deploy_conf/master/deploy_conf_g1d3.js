d4a33ad6e1f960b50851f333f56238688857d0c5
xujianhui <xujianhui@sequoiadb.com>
2024-07-13 06:01:31 +0000

    Update deploy_conf_g1d3.js: enable mongroupmask

--- ansible/roles/sdb_deploy/files/deploy_conf/master/deploy_conf_g1d3.js
+++ ansible/roles/sdb_deploy/files/deploy_conf/master/deploy_conf_g1d3.js
@@ -35,14 +35,16 @@ var cataConf  = { diaglevel:diagLevel,
                   diagnum:60,
                   ftfusingtimeout:300,
                   logfilenum:40,
-                  netcompressor:'LZ4'
+                  netcompressor:'LZ4',
+                  mongroupmask:'all:detail'
                 };
 var coordConf = { diaglevel:diagLevel,
                   diagnum:60,
                   logfilenum:40,
                   ftfusingtimeout:300,
                   fap:fapValue,
-                  netcompressor:'LZ4'
+                  netcompressor:'LZ4',
+                  mongroupmask:'all:detail'
                 };
 var dataConf  = { diaglevel:diagLevel,
                   sharingbreak:30000,
@@ -51,5 +53,6 @@ var dataConf  = { diaglevel:diagLevel,
                   logfilenum:40,
                   indexpath: METADATA_DIR + '/[svcname]/',
                   lobmetapath: METADATA_DIR + '/[svcname]/',
-                  netcompressor:'LZ4'
+                  netcompressor:'LZ4',
+                  mongroupmask:'all:detail'
                 };
